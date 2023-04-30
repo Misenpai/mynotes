c0f0e629ee6cbbab2c9c87e023248a5343e68654
theanimesmit <sumitsinha200231@gmail.com>
2023-04-30 11:23:17 +0530

    step -12

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 @immutable
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser({required this.isEmailVerified});
+  final String? email;
+  const AuthUser({required this.isEmailVerified, required this.email});
   factory AuthUser.fromFirebase(User user) =>
-      AuthUser(isEmailVerified: user.emailVerified);
+      AuthUser(isEmailVerified: user.emailVerified, email: user.email);
 }
